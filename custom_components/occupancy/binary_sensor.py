d65bec8ebb05273fd42e809f8178b656730f7642
Aephir <waldenb@gmail.com>
2023-08-12 15:09:07 +0200

    Fixed index error

--- custom_components/occupancy/binary_sensor.py
+++ custom_components/occupancy/binary_sensor.py
@@ -104,7 +104,7 @@ class HomeOccupancyBinarySensor(Entity):
     async def async_update(self) -> None:
         """Update binary_sensor"""
 
-        presence_sensors: list[str] = [self.config[f"sensor_{i + 1}"][PRESENCE_SENSOR] for i in range(len(self.config))]
+        presence_sensors: list[str] = [self.config[key][PRESENCE_SENSOR] for key in self.config if key.startswith("sensor_")]
 
         home_handle = async_track_state_change(
             self.hass,
